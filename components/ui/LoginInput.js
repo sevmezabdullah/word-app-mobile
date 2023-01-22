dc62bdb5f8bd0a50ddd33b5fb983a68c24ae5973
sevmezabdullah <abdullahsevmez@gmail.com>
2023-01-22 21:52:19 +0300

    screens and components created

---
+++ components/ui/LoginInput.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const LoginInput = ({ placeHolder, value, setValue }) => {
+  return (
+    <View>
+      <Text>LoginInput</Text>
+    </View>
+  );
+};
+
+export default LoginInput;
+
+const styles = StyleSheet.create({});
---
+++ components/ui/SignButton.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const SignButton = ({ icon, color, size, onPress }) => {
+  return (
+    <View>
+      <Text>SignButton</Text>
+    </View>
+  );
+};
+
+export default SignButton;
+
+const styles = StyleSheet.create({});
